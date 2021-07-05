a0cbf776f20406be3b29b7a1f0c506cb2be4920c
nibble0101 <mj.mawa@yahoo.com>
2021-07-05 23:32:30 +0300

    Add more code

--- netlify/functions/submission-created.js
+++ netlify/functions/submission-created.js
@@ -1,8 +1,20 @@
 exports.handler = async function (event, context) {
-  const { body } = event;
-  console.log(JSON.parse(body));
-  return {
-    statusCode: 200,
-    body: JSON.stringify({ message: "Hello world" }),
-  };
+  if (event.httpMethod !== "POST") {
+    return { statusCode: 405, message: "Method Not Allowed" };
+  }
+  try {
+    const { payload } = JSON.parse(event.body);
+    const { name, email, message } = payload.data;
+    // Send email from here
+    console.log(`name: ${name}`);
+    console.log(`email: ${email}`);
+    console.log(`message: ${message}`);
+    return {
+      statusCode: 200,
+      body: JSON.stringify({ message: "Hello world" }),
+    };
+  } catch (err) {
+    console.log(err);
+    return { statusCode: 500, message: err.toString() };
+  }
 };
