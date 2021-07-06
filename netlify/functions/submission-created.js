3a70f66436d91fe7483f8e8b91d3b15a89fe997f
nibble0101 <mj.mawa@yahoo.com>
2021-07-06 18:57:51 +0300

    Add logic for sending emails

--- netlify/functions/submission-created.js
+++ netlify/functions/submission-created.js
@@ -1,3 +1,4 @@
+const sendGridMail = require("@sendgrid/mail");
 exports.handler = async function (event, context) {
   if (event.httpMethod !== "POST") {
     return {
@@ -6,15 +7,21 @@ exports.handler = async function (event, context) {
     };
   }
   try {
+    sendGridMail.setApiKey(process.env.SENDGRID_API_KEY);
     const { payload } = JSON.parse(event.body);
     const { name, email, message } = payload.data;
-    // Send email from here
-    console.log(`name: ${name}`);
-    console.log(`email: ${email}`);
-    console.log(`message: ${message}`);
+    const mail = {
+      from: process.env.SENDER_EMAIL,
+      to: email,
+      subject: "We have received your message",
+      text: `Hi ${name}! Thanks for getting in touch. This is to acknowledge receipt of your message`,
+      html: `<p>Hi ${name}! <br> Thanks for getting in touch. This is to acknowledge receipt of your email</p>`,
+    };
+    console.log(message)
+    sendGridMail.send(mail);
     return {
       statusCode: 200,
-      body: JSON.stringify({ message: "Hello world" }),
+      body: JSON.stringify({ message: "Email sent" }),
     };
   } catch (err) {
     console.log(err);
